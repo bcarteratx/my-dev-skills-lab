d616a16c12960bfe11a3c9df86a9a79ac03511b7
Brandon Carter <bcarteratx@gmail.com>
2020-02-08 22:25:47 -0600

    solve AAU 1 and 3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 // As a User (AAU), I don't want to see any developer skills when the page first loads so that I can start with a fresh slate.
+$('td').hide();
+
 // AAU, I want to be able to type in a skill and have it added to my list of skills by clicking a button.
+
+
 // AAU, I want to be able to remove an individual skill one at a time in case I make a mistake.
+$('tr').on('click', function(evt) {
+    $(this).remove();    
+  });
 
