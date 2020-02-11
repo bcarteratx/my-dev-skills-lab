4498187f0d296135cc600022d219998fc22acfd3
Brandon Carter <bcarteratx@gmail.com>
2020-02-10 20:08:43 -0600

    finish styling

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@
 $('td').hide();
 
 // AAU, I want to be able to type in a skill and have it added to my list of skills by clicking a button.
-
+$('#addSkill').on('click', function() {
+     let skill = $('input').val();
+     let newSkill =
+     `<tr>
+       <td><button class="remove">X</button></td>
+       <td class="skills">${skill}</td>
+     </tr>`
+     $('tbody').append(newSkill);
+     $('input').val('');
+ });
 
 // AAU, I want to be able to remove an individual skill one at a time in case I make a mistake.
-$('tr').on('click', function(evt) {
-    $(this).remove();    
-  });
-
+   $('tbody').on('click', 'button', function() {
+       $(this).closest('tr').fadeOut(500, function() {
+           $(this).remove();
+       });
+   });
